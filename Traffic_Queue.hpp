8af0875492f35df96a6d25889fa64aba9a4ddb69
Maria Alejandra <m.alejandra.taborda@gmail.com>
2018-06-03 17:11:59 -0700

    updated heading to one direction only

--- Traffic_Queue.hpp
+++ Traffic_Queue.hpp
@@ -20,16 +20,20 @@ class Traffic_Queue
 {
 private:
     deque<class User> users_in_queue;
-    pair<string, string> heading;
+    // pair<string, string> heading;
+    string heading;
     string mode;
     double total_wait_time;
 
 public:
     Traffic_Queue();
-    Traffic_Queue(pair<string, string> h, string m);
+    //Traffic_Queue(pair<string, string> h, string m);
+    Traffic_Queue(string h, string m);
     int getSize();
-    void setHeading(string, string);
-    pair<string, string> getHeading();
+    //void setHeading(string, string);
+    void setHeading(string);
+    //pair<string, string> getHeading();
+    string getHeading();
     void setMode(string);
     string getMode();
     void setTotalWaitTime(double);
