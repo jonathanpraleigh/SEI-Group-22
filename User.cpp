310d8526c070bc8b404581a6319805a4f37b167a
Kaitlin Lynch <33095641+kalynch@users.noreply.github.com>
2018-06-08 17:09:49 -0700

    Final updates for Input

--- User.cpp
+++ User.cpp
@@ -1,7 +1,7 @@
 /**********************************************************************
  ** Project: SEI Group 22
  ** File name: User.cpp
- ** Last Updated by Kaitlin Lynch on 6/5/18
+ ** Last Updated by Kaitlin Lynch and James Palen on 6/8/18
  ** Description: method file for the User class. Users will begin as
  vehicles but may also be pedestrians, bikes, public transport, etc.
  in future iterations.
@@ -18,6 +18,7 @@ User::User ()
 {
     id = num_users();
     heading = "unknown";
+	origin = "unknown";
     mode = "unknown";
     type = "unknown";
     time_arrived = 0;
@@ -30,10 +31,11 @@ User::User ()
  time_arrived to time first detected by sensor in seconds. Accounts for
  any delay when sending information from sensors to system.
  **********************************************************************/
-User::User (string h, string m, string t, double ta, int s)
+User::User(string h, string o, string m, string t, double ta, int s)
 {
     id = num_users();
     heading = h;
+	origin = o;
     mode = m;
     type = t;
     time_arrived = ta;
@@ -62,6 +64,15 @@ void User::setHeading (string h)
 string User::getHeading()
 {
     return heading;
+}
+
+void User::setOrigin(string o)
+{
+	origin = o;
+}
+string User::getOrigin()
+{
+	return origin;
 }
 
 void User::setMode (string m)
--- User.hpp
+++ User.hpp
@@ -1,7 +1,7 @@
 /**********************************************************************
  ** Project: SEI Group 22
  ** File name: User.hpp
- ** Last Updated by Kaitlin Lynch on 6/5/18
+ ** Last Updated by Kaitlin Lynch and James Palen on 6/8/18
  ** Description: header file for the User class. Users will begin as
  vehicles but may also be pedestrians, bikes, public transport, etc.
  in future iterations.
@@ -25,6 +25,7 @@ private:
     }
     int id;                            // unique user id;
     string heading;                    // direction user is heading, determined by lane
+	string origin;						//Direction user is coming from
     string mode;                       // mode of transportation (e.g., vehicle, pedestrian, bike)
     string type;                       // type of override user (e.g., emergency, public_transport)
     double time_arrived;               // time (seconds) when user detected entering system
@@ -32,11 +33,13 @@ private:
 
 public:
     User ();
-    User (string h, string m, string t, double ta, int s);
+    User (string h, string o, string m, string t, double ta, int s);
     void setId(int);
     int getId();
     void setHeading (string h);
     string getHeading();
+	void setOrigin(string h);
+	string getOrigin();
     void setMode (string m);
     string getMode();
     void setType (string t);
